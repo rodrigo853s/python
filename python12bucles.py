304ff453b0c12bd43f27b169f4a0bb2df95f3017
rodrigo853s <nsswitch.conf@gmail.com>
2025-02-10 20:52:20 +0100

    Rest Lunes 10 Feb

---
+++ python12bucles.py
@@ -0,0 +1,13 @@
+print("WHILE")
+# DE MOMENTO, SOLO UN CONTADOR
+# NECESITAMOS UNA VAR PARA LA CONDICION DEL BUCLE
+contador = 0
+#while (contador <= 5):
+ #   print("Bucle infinito")
+#print("Fin del programa")
+
+while (contador <= 5):
+    print("Contador", contador)
+# DEBEMOS INDICAR QUE SALDREMOS DEL BUCLE
+    contador = contador +1
+print("Fin del programa")
