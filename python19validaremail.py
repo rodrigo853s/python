75c2d5232e345035185099500be36a593b4b10b8
rodrigo853s <nsswitch.conf@gmail.com>
2025-02-12 20:53:23 +0100

    Miércoles

--- python19validaremail.py
+++ python19validaremail.py
@@ -6,28 +6,33 @@
 # MENSAJE CON EL ERROR DETECTADO
 print("Introduzca email...")
 email = input()
-position = email.find('@')
-print(position)
-if (email.find("@") == -1 ):
+
+if (email.find("@") == -1 ): # email.count("@") == 0
     print("El email no tiene @ ")
-elif (email.startswith("@") == 1 or email.endswith("@")):
+elif (email.startswith("@") == 1 or email.endswith("@") == 1):
     print("El email no puede empezar o terminar con @ ")
 elif (email.count("@") > 1):
     print("El email tiene más de una @")
 
 if (email.find(".") == -1 ):
     print("El email no tiene . ")
-elif (email.startswith(".") == 1 or email.endswith(".")):
+elif (email.startswith(".") == 1 or email.endswith(".") == 1):
     print("El email no puede empezar o terminar con . ")
-
+# EXISTA PUNTO DESPUES DE ARROBA
+position = email.find('@')
 position2 = email.rfind('.')
-print(position2)
+
 if (position2 < position):
     print("No hay . después de @")
+# NECESITAMOS RECUPERAR EL DOMINIO A PARTIR DEL ÚLTIMO PUNTO
 
-#if (email.rfind('.') > 3)
-print(email.rfind('.'))
-punto = (email.rfind('.'))
+ultimoPunto = (email.rfind('.'))
+dominio = email[ultimoPunto + 1:]
+longitudDominio = len(dominio)
+if ( longitudDominio > 3 or longitudDominio < 2):
+    print("Dominio mal..")
+print("Fin de programa")
+
 
 
 
