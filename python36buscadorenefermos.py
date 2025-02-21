6fc13dc5e311bd80c13d6b7b9cb6edc09396647a
rodrigo853s <nsswitch.conf@gmail.com>
2025-02-21 19:30:12 +0100

    Viernes 21

---
+++ python36buscadorenefermos.py
@@ -0,0 +1,35 @@
+# Buscar un enfermo por su INSCRIPCIÓN
+# Si no existe, lo indicamos. Si existe,
+# mostramos apellido y dirección
+
+import oracledb
+
+connection = oracledb.connect(user='SYSTEM', password='oracle', dsn='localhost/xe')
+
+print("Conectados!!!")
+print("Introduzca número de inscripcion")
+data = input() # Lo cogemos como texto, para pasárselo al string de la consulta sql
+
+
+cursor = connection.cursor()
+sql = "select * from ENFERMO where inscripcion=" + data
+cursor.execute(sql)
+
+# Como estamos buscando por Primary key
+# sólo nos puede devolver una fila
+
+row = cursor.fetchone()
+
+# Debemos comprobar si la fila tiene contenido/algo
+
+if (not row):
+    print("No existe el enfermo")
+else:
+    apellido = row[1]
+    direccion = row[2]
+    print(apellido + ", " + direccion )
+
+# Cerrar TODO
+
+cursor.close()
+connection.close()
